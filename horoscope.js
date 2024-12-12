5659cbe1b96b7869cb7f16322b7bc703e744f4b3
Emi1-5 <ebayala@arizona.edu>
2024-12-11 20:24:58 -0700

    Renamed Fortunes.js to horoscope.js and updated eight_ball.js

--- horoscope.js
+++ horoscope.js
@@ -74,28 +74,6 @@ const cookie_fortunes = [
     "Your ability for accomplishment will follow with success."
 ];
 
-const eight_ball_fortunes = [
-    "It is certain",
-    "Reply hazy, try again",
-    "Don’t count on it",
-    "It is decidedly so",
-    "Ask again later",
-    "My reply is no",
-    "Without a doubt",
-    "Better not tell you now",
-    "My sources say no",
-    "Yes definitely",
-    "Cannot predict now",
-    "Outlook not so good",
-    "You may rely on it",
-    "Concentrate and ask again",
-    "Very doubtful",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes"
-]
 
 let rand_index = Math.floor(Math.random() * cookie_fortunes.length)
 
